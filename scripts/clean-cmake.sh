a9b6d915c202dcb2b72602ac202692ee600aa371
kds285 <kds285@gmail.com>
2022-12-14 15:24:26 +0800

    update clean-cmake.sh

--- scripts/clean-cmake.sh
+++ scripts/clean-cmake.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Clean up cmake files ... "
-rm -rf detect_cuda_version.cc detect_cuda_compute_capabilities.cpp CMakeCache.txt CMakeFiles Makefile cmake_install.cmake 
+rm -rf detect_cuda_version.cc detect_cuda_compute_capabilities.cu detect_cuda_compute_capabilities.cpp CMakeCache.txt CMakeFiles Makefile cmake_install.cmake 
 for cmake_dir in $(grep "add_subdirectory" CMakeLists.txt | awk -F "\(|\)" '{ print $2; }')
 do
     rm -rf ${cmake_dir}/CMakeFiles ${cmake_dir}/Makefile ${cmake_dir}/cmake_install.cmake ${cmake_dir}/*.so
