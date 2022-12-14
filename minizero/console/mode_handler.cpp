194dc8b9818916106ca09a5941988af405ab11ef
kds285 <kds285@gmail.com>
2022-12-14 13:44:22 +0800

    refactor main.cpp by adding mode handler

---
+++ minizero/console/mode_handler.cpp
@@ -0,0 +1,150 @@
+#include "mode_handler.h"
+#include "actor_group.h"
+#include "console.h"
+#include "random.h"
+#include "zero_server.h"
+
+namespace minizero::console {
+
+using namespace minizero::utils;
+
+ModeHandler::ModeHandler()
+{
+    RegisterFunction("console", this, &ModeHandler::runConsole);
+    RegisterFunction("sp", this, &ModeHandler::runSelfPlay);
+    RegisterFunction("zero_server", this, &ModeHandler::runZeroServer);
+    RegisterFunction("env_test", this, &ModeHandler::runEnvTest);
+}
+
+void ModeHandler::run(int argc, char* argv[])
+{
+    if (argc % 2 != 1) { usage(); }
+
+    env::setUpEnv();
+
+    std::string mode_string = "console";
+    std::string config_file = "";
+    std::string config_string = "";
+    config::ConfigureLoader cl;
+    setDefaultConfiguration(cl);
+
+    for (int i = 1; i < argc; i += 2) {
+        std::string sCommand = std::string(argv[i]);
+
+        if (sCommand == "-mode") {
+            mode_string = argv[i + 1];
+        } else if (sCommand == "-gen") {
+            genConfiguration(cl, argv[i + 1]);
+            exit(0);
+        } else if (sCommand == "-conf_file") {
+            config_file = argv[i + 1];
+        } else if (sCommand == "-conf_str") {
+            config_string = argv[i + 1];
+        } else {
+            std::cerr << "unknown argument: " << sCommand << std::endl;
+            usage();
+        }
+    }
+
+    if (!readConfiguration(cl, config_file, config_string)) { exit(-1); }
+    utils::Random::seed(config::program_auto_seed ? static_cast<int>(time(NULL)) : config::program_seed); // setup random seed
+    if (!function_map_.count(mode_string)) { usage(); }
+    (*function_map_[mode_string])();
+}
+
+void ModeHandler::usage()
+{
+    std::cout << "./minizero [arguments]" << std::endl;
+    std::cout << "arguments:" << std::endl;
+    std::cout << "\t-mode [" << getAllModesString() << "]" << std::endl;
+    std::cout << "\t-gen configuration_file" << std::endl;
+    std::cout << "\t-conf_file configuration_file" << std::endl;
+    std::cout << "\t-conf_str configuration_string" << std::endl;
+    exit(-1);
+}
+
+std::string ModeHandler::getAllModesString()
+{
+    std::string mode_string;
+    for (const auto& m : function_map_) { mode_string += (mode_string.empty() ? "" : "|") + m.first; }
+    return mode_string;
+}
+
+void ModeHandler::genConfiguration(config::ConfigureLoader& cl, const std::string& config_file)
+{
+    // check configure file is exist
+    std::ifstream f(config_file);
+    if (f.good()) {
+        char ans = ' ';
+        while (ans != 'y' && ans != 'n') {
+            std::cerr << config_file << " already exist, do you want to overwrite it? [y/n]" << std::endl;
+            std::cin >> ans;
+        }
+        if (ans == 'y') { std::cerr << "overwrite " << config_file << std::endl; }
+        if (ans == 'n') {
+            std::cerr << "didn't overwrite " << config_file << std::endl;
+            f.close();
+            return;
+        }
+    }
+    f.close();
+
+    std::ofstream fout(config_file);
+    fout << cl.toString();
+    fout.close();
+}
+
+bool ModeHandler::readConfiguration(config::ConfigureLoader& cl, const std::string& config_file, const std::string& config_string)
+{
+    if (!config_file.empty() && !cl.loadFromFile(config_file)) {
+        std::cerr << "Failed to load configuration file." << std::endl;
+        return false;
+    }
+    if (!config_string.empty() && !cl.loadFromString(config_string)) {
+        std::cerr << "Failed to load configuration string." << std::endl;
+        return false;
+    }
+
+    if (!config::program_quiet) { std::cerr << cl.toString(); }
+    return true;
+}
+
+void ModeHandler::runConsole()
+{
+    console::Console console;
+    std::string command;
+    while (getline(std::cin, command)) {
+        if (command == "quit") { break; }
+        console.executeCommand(command);
+    }
+}
+
+void ModeHandler::runSelfPlay()
+{
+    actor::ActorGroup ag;
+    ag.run();
+}
+
+void ModeHandler::runZeroServer()
+{
+    zero::ZeroServer server;
+    server.run();
+}
+
+void ModeHandler::runEnvTest()
+{
+    Environment env;
+    env.reset();
+    while (!env.isTerminal()) {
+        std::vector<Action> legal_actions = env.getLegalActions();
+        int index = rand() % legal_actions.size();
+        env.act(legal_actions[index]);
+    }
+    std::cout << env.toString() << std::endl;
+
+    EnvironmentLoader env_loader;
+    env_loader.loadFromEnvironment(env);
+    std::cout << env_loader.toString() << std::endl;
+}
+
+} // namespace minizero::console
---
+++ minizero/console/mode_handler.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include "configuration.h"
+#include <map>
+#include <memory>
+
+namespace minizero::console {
+
+class ModeHandler {
+public:
+    ModeHandler();
+
+    void run(int argc, char* argv[]);
+
+protected:
+    class BaseFunction {
+    public:
+        virtual ~BaseFunction() = default;
+        virtual void operator()() = 0;
+    };
+
+    template <class I, class F>
+    class Function : public BaseFunction {
+    public:
+        Function(I* instance, F function) : instance_(instance), function_(function) {}
+        void operator()() { (*instance_.*function_)(); }
+
+        I* instance_;
+        F function_;
+    };
+
+    template <class I, class F>
+    void RegisterFunction(const std::string& name, I* instance, F function)
+    {
+        function_map_[name] = std::make_shared<Function<I, F>>(instance, function);
+    }
+
+    void usage();
+    std::string getAllModesString();
+    virtual void setDefaultConfiguration(config::ConfigureLoader& cl) { config::setConfiguration(cl); }
+    void genConfiguration(config::ConfigureLoader& cl, const std::string& sConfigFile);
+    bool readConfiguration(config::ConfigureLoader& cl, const std::string& sConfigFile, const std::string& sConfigString);
+    virtual void runConsole();
+    virtual void runSelfPlay();
+    virtual void runZeroServer();
+    virtual void runEnvTest();
+
+    std::map<std::string, std::shared_ptr<BaseFunction>> function_map_;
+};
+
+} // namespace minizero::console
