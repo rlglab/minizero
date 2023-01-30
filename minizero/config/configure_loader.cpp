0cc852243e4ae3c45088299d282fa33b775f36dd
kds285 <kds285@gmail.com>
2023-01-30 19:12:15 +0800

    ignore tab and whitespace in configure loader

--- minizero/config/configure_loader.cpp
+++ minizero/config/configure_loader.cpp
@@ -75,8 +75,8 @@ std::string ConfigureLoader::toString() const
 void ConfigureLoader::trim(std::string& s)
 {
     if (s.empty()) { return; }
-    s.erase(0, s.find_first_not_of(" "));
-    s.erase(s.find_last_not_of(" ") + 1);
+    s.erase(0, s.find_first_not_of(" \t"));
+    s.erase(s.find_last_not_of(" \t") + 1);
 }
 
 bool ConfigureLoader::setValue(std::string line)
