274c02854b196e910ba134ffa64eb85f93daf456
kds285 <kds285@gmail.com>
2023-02-24 17:59:35 +0800

    support building all existing games at once

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,28 +4,46 @@ set -e
 support_games=("atari" "chess" "go" "gomoku" "hex" "killallgo" "othello" "tictactoe")
 
 usage() {
-	echo "Usage: build.sh games build_type"
-	echo "  games: $(echo ${support_games[@]} | sed 's/ /, /g')"
+	echo "Usage: build.sh game_type build_type"
+	echo "  game_type: $(echo ${support_games[@]} | sed 's/ /, /g')"
 	echo "  build_type: release(default), debug"
 	exit 1
+}
+
+build_game() {
+	# check arguments is vaild
+	game_type=${1,,}
+	build_type=$2
+	[[ "${support_games[*]}" =~ "${game_type}" ]] || usage
+	[ "${build_type}" == "Debug" ] || [ "${build_type}" == "Release" ] || usage
+
+	# build and make
+	echo "game type: ${game_type}"
+	echo "build type: ${build_type}"
+	if [ ! -d "build/${game_type}" ]; then
+		mkdir -p build/${game_type}
+		cd build/${game_type}
+		cmake ../../ -DCMAKE_BUILD_TYPE=${build_type} -DGAME_TYPE=${game_type^^}
+	else
+		cd build/${game_type}
+	fi
+	make -j$(nproc --all)
+	cd ../..
 }
 
 # add environment settings
 git config core.hooksPath .githooks
 
-# check game and build type
-[ $# -ge 1 ] || usage
-game_type=${1,,}
+game_type=${1:-all}
 build_type=${2:-release}
 build_type=$(echo ${build_type:0:1} | tr '[:lower:]' '[:upper:]')$(echo ${build_type:1} | tr '[:upper:]' '[:lower:]')
-[[ "${support_games[*]}" =~ "${game_type}" ]] || usage
-[ "${build_type}" == "Debug" ] || [ "${build_type}" == "Release" ] || usage
+[ "${game_type}" == "all" ] && [ ! -d "build" ] && usage
 
-# build and make
-echo "game type: ${game_type}"
-echo "build type: ${build_type}"
-[ ! -d "build" ] && mkdir build
-[ ! -d "build/${game_type}" ] && mkdir build/${game_type}
-cd build/${game_type}
-cmake ../../ -DCMAKE_BUILD_TYPE=${build_type} -DGAME_TYPE=${game_type^^}
-make -j$(nproc --all)
+if [ "${game_type}" == "all" ]; then
+	for game in build/*
+	do
+		build_game $(basename ${game}) ${build_type}
+	done
+else
+	build_game ${game_type} ${build_type}
+fi
