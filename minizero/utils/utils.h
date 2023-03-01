b652de67a30b53637bd5bac08f81a3c8e63c3aef
kds285 <kds285@gmail.com>
2023-03-01 20:11:39 +0800

    add function for string compression

--- minizero/utils/utils.h
+++ minizero/utils/utils.h
@@ -1,5 +1,10 @@
 #pragma once
 
+#include <algorithm>
+#include <boost/iostreams/copy.hpp>
+#include <boost/iostreams/filter/gzip.hpp>
+#include <boost/iostreams/filtering_streambuf.hpp>
+#include <iomanip>
 #include <sstream>
 #include <string>
 #include <vector>
@@ -13,6 +18,60 @@ inline std::vector<std::string> stringToVector(const std::string& s)
     std::vector<std::string> args;
     while (parser >> tmp) { args.push_back(tmp); }
     return args;
+}
+
+inline std::string compressToBinaryString(const std::string& s)
+{
+    // use gzip to compress string
+    std::stringstream compressed;
+    boost::iostreams::filtering_streambuf<boost::iostreams::output> out;
+    out.push(boost::iostreams::gzip_compressor());
+    out.push(compressed);
+    boost::iostreams::copy(boost::iostreams::basic_array_source<char>{s.data(), s.size()}, out);
+    boost::iostreams::close(out);
+    return compressed.str();
+}
+
+inline std::string binaryToHexString(const std::string& s)
+{
+    // encode binary string to hex string
+    std::ostringstream oss;
+    for (size_t i = 0; i < s.size(); ++i) {
+        oss << std::setfill('0') << std::setw(2) << std::hex << static_cast<int>(static_cast<unsigned char>(s[i]));
+    }
+    return oss.str();
+}
+
+inline std::string hexToBinaryString(const std::string& s)
+{
+    assert(s.size() % 2 == 0);
+
+    // decode hex string to binary string
+    std::string decompressed_string;
+    for (size_t i = 0; i < s.size(); i += 2) { decompressed_string += static_cast<unsigned char>(std::stoi(s.substr(i, 2), 0, 16)); }
+    return decompressed_string;
+}
+
+inline std::string decompressBinaryString(const std::string& s)
+{
+    // use gzip to decompress binary string
+    boost::iostreams::filtering_streambuf<boost::iostreams::input> in;
+    in.push(boost::iostreams::gzip_decompressor());
+    in.push(boost::iostreams::basic_array_source<char>(s.data(), s.size()));
+    std::stringstream decompressed;
+    boost::iostreams::copy(in, decompressed);
+    boost::iostreams::close(in);
+    return decompressed.str();
+}
+
+inline std::string compressString(const std::string& s)
+{
+    return binaryToHexString(compressToBinaryString(s));
+}
+
+inline std::string decompressString(const std::string& s)
+{
+    return decompressBinaryString(hexToBinaryString(s));
 }
 
 } // namespace minizero::utils
