164108d5ae87d1eb6457f4d62da43f4a6f61aebd
q12we34rt5 <luke123132112@gmail.com>
2024-03-18 12:29:35 +0800

    add the option '-H' to start-container.sh to enable recording the bash history

--- scripts/start-container.sh
+++ scripts/start-container.sh
@@ -11,6 +11,7 @@ usage()
     echo "  -v, --volume   Bind mount a volume into the container"
     echo "      --name     Assign a name to the container"
     echo "  -d, --detach   Run container in background and print container ID"
+	echo "  -H, --history  Record the container bash history"
     exit 1
 }
 
@@ -22,6 +23,7 @@ if [[ ! $container_tool ]]; then
 fi
 container_volume="-v .:/workspace"
 container_argumenets=""
+record_history=false
 while :; do
 	case $1 in
 		-h|--help) shift; usage
@@ -34,6 +36,8 @@ while :; do
 		;;
 		-d|--detach) container_argumenets="${container_argumenets} -d"
 		;;
+		-H|--history) record_history=true
+		;;
 		"") break
 		;;
 		*) echo "Unknown argument: $1"; usage
@@ -41,6 +45,18 @@ while :; do
 	esac
 	shift
 done
+
+if [ "$record_history" = true ]; then
+	history_dir=".container_root"
+	# if the history directory is not exist, create it and initialize the history directory
+	if [ ! -d ${history_dir} ]; then
+		mkdir -p ${history_dir}
+		# start container with the history directory and copy the history to the history directory
+		$container_tool run --cap-add=SYS_PTRACE --security-opt seccomp=unconfined --rm -it -v "${history_dir}:/container_root" ${image_name} /bin/bash -c "cp -r /root/. /container_root && touch /container_root/.bash_history && exit"
+	fi
+	# add the history directory to the container volume
+	container_volume="${container_volume} -v ${history_dir}:/root"
+fi
 
 container_argumenets=$(echo ${container_argumenets} | xargs)
 echo "$container_tool run ${container_argumenets} --cap-add=SYS_PTRACE --security-opt seccomp=unconfined --network=host --ipc=host --rm -it ${container_volume} ${image_name}"
